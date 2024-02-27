b0a87216dc690bcd173dfca1b8f780d6b78ae3e2
GitHubvipM <vipul9595mamde@gmail.com>
2024-02-27 22:13:07 +0530

    test1

---
+++ main.py
@@ -0,0 +1,24 @@
+print("Hello World")
+print(5)
+# 
+print ("hey",6,7,sep="~",end="9\n")
+print("Hello World")
+print(5+15)
+print(115-15)
+print(5*15)
+print(5/15)
+print(15//5) #floor division
+print(5**2) #Exponential Operator
+
+a=6
+b=2
+c=a+b
+print("Addition of ",a,"and",b,"is",c)
+c=a-b
+print("substraction of ",a,"and",b,"is",c)
+c=a*b
+print("Multiplication  of ",a,"and",b,"is",c)
+c=a/b
+print("Division of ",a,"and",b,"is",c)
+print("Division of ",a,"and",b,"is",c)
+print("Division of ",a,"and",b,"is",c)
